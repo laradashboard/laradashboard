8cd9e8711b08fa32689ed824e4a8a7299c542f53
engrshishir <shishir.cse.pstu@gmail.com>
2025-11-11 13:49:20 +0600

    refactor: format

--- app/Providers/EmailServiceProvider.php
+++ app/Providers/EmailServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Enums\Hooks\SettingFilterHook;
-use App\Support\Facades\Hook;
 use Illuminate\Support\ServiceProvider;
 
 class EmailServiceProvider extends ServiceProvider
